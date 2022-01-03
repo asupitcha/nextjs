dc9aac83c01ded66a9afa49192b467bcf602d3ce
Supitcha Buabang <a.supitcha@hotmail.com>
2022-01-04 00:17:02 +0700

    test nextjs

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,7 @@ export default function Home({ posts }) {
 
   return (
     <div className={styles.container}>
-      {posts.data && posts.data.map((post) => (
-        <div key={post.id}>
-          <h2>{post.attributes.Title}</h2>
-        </div>
-      ))}
+      Test Next.js
     </div>
   )
-}
-
-export async function getStaticProps() {
-  // get data from api
-  const res = await fetch('http://localhost:1337/api/posts?populate=*')
-  const posts = await res.json();
-  return {
-    props: { posts }
-  }
 }
