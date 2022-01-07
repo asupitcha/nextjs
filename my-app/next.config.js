a80d1c6cc776981e673a216f1ecb0eafde18e26f
Supitcha Buabang <supitcha.b@codium.co>
2022-01-07 13:53:07 +0700

    test config

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
+const path = require('path')
+require("dotenv").config();
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    loader: 'imgix',
+    path: ''
+  },
 }
