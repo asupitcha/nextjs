ae84f2167e1fab33ebecaff53c7f40506632d87c
Supitcha Buabang <supitcha.b@codium.co>
2022-01-09 13:32:16 +0700

    remove /

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ courses }) {
         <div key={post.id}>
           <h2>{post.attributes.speaker_name}</h2>
           <Img className="feature-image"
-            src={'https://strapi-dev.cariber.co/' + post.attributes.thumbnail.data.attributes.url}
+            src={'https://strapi-dev.cariber.co' + post.attributes.thumbnail.data.attributes.url}
             alt={post.attributes.thumbnail.data.attributes.name}
             width={262.5}
             height={147.65} />
